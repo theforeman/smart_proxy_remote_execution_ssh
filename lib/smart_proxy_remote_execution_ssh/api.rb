ee9d66837bf9d0b9ffc961c88eeef0f9ec3d49e5
Adam Ruzicka <aruzicka@redhat.com>
2019-11-15 12:59:02 +0100

    Fixes #21449 - Add API for removal of known hosts records

--- lib/smart_proxy_remote_execution_ssh/api.rb
+++ lib/smart_proxy_remote_execution_ssh/api.rb
@@ -1,3 +1,6 @@
+require 'net/ssh'
+require 'base64'
+
 module Proxy::RemoteExecution
   module Ssh
 
@@ -16,6 +19,23 @@ module Proxy::RemoteExecution
         end
         session.hijack!
         101
+      end
+
+      delete '/known_hosts/:name' do |name|
+        do_authorize_any
+        keys = Net::SSH::KnownHosts.search_for(name)
+        return [204] if keys.empty?
+        ssh_keys = keys.map { |key| Base64.strict_encode64 key.to_blob }
+        Net::SSH::KnownHosts.hostfiles({}, :user)
+          .map { |file| File.expand_path file }
+          .select { |file| File.readable?(file) && File.writable?(file) }
+          .each do |host_file|
+            lines = File.foreach(host_file).reject do |line|
+              ssh_keys.any? { |key| line.end_with? "#{key}\n" }
+            end
+            File.open(host_file, 'w') { |f| f.write lines.join }
+          end
+        204
       end
     end
   end
