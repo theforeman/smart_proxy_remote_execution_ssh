7b6b93f0fc9c50c1d1ea39db04a246c9747d8a96
alazik <alazik@redhat.com>
2024-08-27 13:47:49 +0000

    Fixes #36456 - Remove hardcoded ProxyCommand=none

--- lib/smart_proxy_remote_execution_ssh/multiplexed_ssh_connection.rb
+++ lib/smart_proxy_remote_execution_ssh/multiplexed_ssh_connection.rb
@@ -161,7 +161,6 @@ module Proxy::RemoteExecution::Ssh::Runners
       ssh_options << "-o ControlMaster=auto"
       ssh_options << "-o ControlPath=#{socket_file}"
       ssh_options << "-o ControlPersist=yes"
-      ssh_options << "-o ProxyCommand=none"
       ssh_options << "-o ServerAliveInterval=15"
       ssh_options << "-o ServerAliveCountMax=3" # This is the default, but let's be explicit
       @establish_ssh_options = ssh_options
